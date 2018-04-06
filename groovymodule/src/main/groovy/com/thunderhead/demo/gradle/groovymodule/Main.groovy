7b362c2223286208d0bfb8fd830c1ca57b124bb1
jtullgren <jtullgren@thunderhead.com>
2018-04-06 10:46:14 -0700

    Multi project setup.

---
+++ groovymodule/src/main/groovy/com/thunderhead/demo/gradle/groovymodule/Main.groovy
@@ -0,0 +1,13 @@
+package com.thunderhead.demo.gradle.groovymodule
+
+import groovy.transform.CompileStatic
+
+class Main {
+
+    static void main(String[] args) {
+
+        def hello = "hello"
+        println "$hello world!!"
+    }
+
+}
