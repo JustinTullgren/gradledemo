e56227025fdde981e510f6f2ad6b4fb08ec5847e
jtullgren <jtullgren@thunderhead.com>
2018-04-06 12:34:08 -0700

    custom source sets

---
+++ javamodule/customSrc/main/java/com/thunderhead/demo/gradle/javamodule/CustomSrcClass.java
@@ -0,0 +1,7 @@
+package com.thunderhead.demo.gradle.javamodule;
+
+public class CustomSrcClass {
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+}
--- javamodule/src/main/java/com/thunderhead/demo/gradle/javamodule/Main.java
+++ javamodule/src/main/java/com/thunderhead/demo/gradle/javamodule/Main.java
@@ -2,7 +2,6 @@ package com.thunderhead.demo.gradle.javamodule;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello world!");
+        CustomSrcClass.sayHello();
     }
 }
